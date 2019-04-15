15c7d0445f29c229804a5103fe4b3a1a3d3a2a03
jcwise98 <jcwise@udel.edu>
2019-04-14 21:54:56 -0400

    title screen view

--- src/game/TitleScreenView.java
+++ src/game/TitleScreenView.java
@@ -3,13 +3,25 @@ package game;
  * 
  */
 
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
+
 /*Class: TitleScreenView
  * -class that acts as the View of the TitleScreen 
  * -contains methods and control over the drawing of the TitleScreen
  */
 public class TitleScreenView extends GameView {
 	
+	JButton RedKnot;
+	JButton ClapperRail;
+	JButton Instructions;
+	
 	public TitleScreenView() {
-		
+		this.RedKnot = new JButton("RED KNOT");
+		this.ClapperRail = new JButton("CLAPPER RAIL");
+		BorderLayout layout = new BorderLayout();
+		this.setLayout(layout);
+		this.add(RedKnot, BorderLayout.NORTH);
 	}
 }
