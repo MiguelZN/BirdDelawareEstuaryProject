ab74ac1b9d7820b7adfbe05d06660fc548b6272a
MiguelZN <miguelzn>
2019-04-15 00:01:15 -0400

    AddedInstructionFunctionality, clapperrailInstructions, redknotInstructions

--- src/game/InstructionsView.java
+++ src/game/InstructionsView.java
@@ -8,6 +8,7 @@ import java.awt.image.BufferedImage;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.border.EmptyBorder;
 
 /*Authors: Miguel Zavala, Derek Baum, Matt Benvenuto, Jake Wise
  * 
@@ -18,17 +19,21 @@ import javax.swing.JLabel;
  */
 public class InstructionsView extends GameView{
 	ImageIcon titleImage;
-	BufferedImage[] previewImages;
+	ImageIcon redknot;
+	ImageIcon clapperrail;
 	JButton nextButton;
 	JButton backtoMenuButton;
 	JLabel titlelogolabel;
+	JLabel currentInstructions;
+	Controller controller;
+	
+	BirdType current_bird_info;
+	
 	
 	public ImageIcon getTitleImage() {
 		return titleImage;
 	}
-	public BufferedImage[] getPreviewImages() {
-		return previewImages;
-	}
+	
 	public JButton getNextButton() {
 		return nextButton;
 	}
@@ -39,6 +44,12 @@ public class InstructionsView extends GameView{
 	public InstructionsView(Controller c) {
 		super(c);
 		this.nextButton = new JButton("next");
+		this.nextButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				showcaseNextInstructions();
+			}
+		});
+		
 		this.backtoMenuButton = new JButton("Back-to-Menu");
 		this.backtoMenuButton.addActionListener(new ActionListener() {
 
@@ -47,21 +58,50 @@ public class InstructionsView extends GameView{
 		        c.changeView(GameMode.TITLESCREEN);
 		    }
 		});
-//		this.nextButton.setSize(75, 30);
-//		this.backtoMenuButton.setSize(75,30);
+		this.nextButton.setSize(75, 30);
+		this.backtoMenuButton.setSize(75,30);
 		BorderLayout layout = new BorderLayout();
 		this.setLayout(layout);
 		
 		ImageIcon titleiconLogo = new ImageIcon("resources/images/instructions.png");
 		this.titlelogolabel = new JLabel(titleiconLogo);
 		this.titlelogolabel.setSize(200, 50);
+
+		this.currentInstructions = new JLabel(clapperrail);
+		this.currentInstructions.setSize(300, 300);
+		this.currentInstructions.setBorder(new EmptyBorder(10,10,10,10));
+		this.current_bird_info = BirdType.REDKNOT;
+		
 		
 		this.add(titlelogolabel, BorderLayout.PAGE_START);
-		this.add(nextButton, BorderLayout.EAST);
+		this.add(this.nextButton, BorderLayout.EAST);
+		this.add(this.currentInstructions, BorderLayout.WEST);
 		this.add(backtoMenuButton, BorderLayout.PAGE_END);
 		this.titlelogolabel.setVisible(true);
 		System.out.println("TITLELOGO");
+		
+		showcaseNextInstructions();
 	}
 	
+	public void showcaseNextInstructions() {
+		if(this.current_bird_info==BirdType.REDKNOT) {
+			this.current_bird_info = BirdType.CLAPPERRAIL;
+		}
+		else {
+			this.current_bird_info = BirdType.REDKNOT;
+		}
+		
+		switch(this.current_bird_info) {
+		case REDKNOT:
+			this.currentInstructions.setIcon(new ImageIcon("resources/images/instructionsredknot.png"));
+			break;
+		
+		case CLAPPERRAIL:
+			this.currentInstructions.setIcon(new ImageIcon("resources/images/clapperrailinstructions.png"));
+			break;
+		}
+		
+		this.currentInstructions.setVisible(true);
+	}
 	
 }
